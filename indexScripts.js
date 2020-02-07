954adeea2fa1d6b2879f5d5dc7afe3f49775fb6f
Ian Jarvis <ijarvis12@gmail.com>
2020-02-07 17:29:40 -0600

    Adding username password help

---
+++ indexScripts.js
@@ -0,0 +1,20 @@
+var noAccnt = document.getElementById("noAccount");
+var forgotPass = document.getElementById("forgotPassword");
+
+function noAccount() {
+  if(noAccnt.style.display == "none" && forgotPass.style.display == "none") {
+    noAccnt.style.display = "block";
+  }
+  else {
+    noAccnt.style.display = "none";
+  }
+}
+
+function forgotPassword() {
+  if(forgotPass.style.display == "none" && noAccnt.style.display == "none") {
+    forgotPass.style.display = "block";
+  }
+  else {
+    forgotPass.style.display = "none";
+  }
+}
